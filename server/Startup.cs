9dfb96fd73bf875348f2305de2e5dbb2bd9207ab
Will Germany <germany.william@gmail.com>
2018-04-01 20:19:43 -0400

    Update

--- server/Startup.cs
+++ server/Startup.cs
@@ -7,15 +7,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace MyFirstNETCoreWebAPI
+namespace Server
 {
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+        public void ConfigureServices(IServiceCollection services) => services.AddMvc();
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -25,9 +23,11 @@ namespace MyFirstNETCoreWebAPI
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseMvc();
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("MVC not used!");
             });
         }
     }
